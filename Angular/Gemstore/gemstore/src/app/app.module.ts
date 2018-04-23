ea2757543572cacbe39288521551f0c166fd0cb1
Sean Frizzell <skeltonic@gmail.com>
2018-04-23 15:13:15 -0500

    navbar
    navbar

--- Angular/Gemstore/gemstore/src/app/app.module.ts
+++ Angular/Gemstore/gemstore/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GemComponent } from './gem/gem.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GemComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ Angular/Gemstore/gemstore/src/app/gem/gem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gem',
+  templateUrl: './gem.component.html',
+  styleUrls: ['./gem.component.css']
+})
+export class GemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
